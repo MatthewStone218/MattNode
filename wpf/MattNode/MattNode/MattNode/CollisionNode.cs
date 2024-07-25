56807185526ff66cedfb60b6665438c4a6276a73
matt <matt@physia.kr>
2024-07-25 15:30:43 +0900

    쿼드트리 만드려고 준비중

---
+++ wpf/MattNode/MattNode/MattNode/CollisionNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MattNode
+{
+    public partial class CollisionNode
+    {
+        public double X,Y,Width,Height;
+        public CollisionNode[] Nodes = [];
+        public CollisionNode[] Nodes = [];
+        public CollisionNode(double _X,double _Y,double _Width,double _Height)
+        {
+            X = _X;
+            Y = _Y;
+            Width = _Width;
+            Height = _Height;
+        }
+
+    }
+}
---
+++ wpf/MattNode/MattNode/MattNode/Instance.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MattNode
+{
+    /// <summary>
+    /// Instance.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Instance : UserControl
+    {
+        public Instance()
+        {
+            InitializeComponent();
+        }
+
+        public bool Interacts(Instance other)
+        {
+            return !(
+                Margin.Left > other.Margin.Left+other.Width ||
+                Margin.Right+Width < other.Margin.Left ||
+                Margin.Top > other.Margin.Top+other.Height ||
+                Margin.Top+Height < other.Margin.Top
+                );
+        }
+    }
+}
