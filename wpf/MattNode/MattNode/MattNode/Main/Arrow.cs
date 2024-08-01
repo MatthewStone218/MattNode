18b0e1cc914c6b69bd64e17ae269d2a303cbb6b1
matt <matt@physia.kr>
2024-08-01 18:13:46 +0900

    화살표

---
+++ wpf/MattNode/MattNode/MattNode/Main/Arrow.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MattNode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+    /// <summary>
+    /// Arrow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Arrow : Instance
+    {
+        protected Line ArrowLine;
+        protected Line ArrowHead1;
+        protected Line ArrowHead2;
+        public Arrow()
+        {
+        }
+
+        public void SetArrow(double x1, double y1, double x2, double y2)
+        {
+            if (x1 < x2)
+            {
+                Canvas.SetLeft(this, x1 - 5);
+                Width = x2 - x1 + 10;
+            }
+            else
+            {
+                Canvas.SetLeft(this, x2 - 5);
+                Width = x1 - x2 + 10;
+            }
+
+            if (y1 < y2)
+            {
+                Canvas.SetTop(this, y1 - 5);
+                Height = y2 - y1 + 10;
+            }
+            else
+            {
+                Canvas.SetTop(this, y2 - 5);
+                Height = y1 - y2 + 10;
+            }
+
+            Point startPoint = GetLocalPosFromMainCanvas(new Point(x1, y1));
+            Point endPoint = GetLocalPosFromMainCanvas(new Point(x2, y2));
+
+            ArrowLine.X1 = startPoint.X;
+            ArrowLine.Y1 = startPoint.Y;
+            ArrowLine.X2 = endPoint.X;
+            ArrowLine.Y2 = endPoint.Y;
+            
+            ArrowHead1.X1 = endPoint.X;
+            ArrowHead1.Y1 = endPoint.Y;
+
+            ArrowHead1.X2 = ArrowHead1.X1 + Math.Cos(Math.Atan((ArrowLine.Y2 - ArrowLine.Y1) / (ArrowLine.X2 - ArrowLine.X1)) + (x1 < x2 ? (Math.PI * 3 / 4) : (Math.PI * 1 / 4))) * 15;
+            ArrowHead1.Y2 = ArrowHead1.Y1 + Math.Sin(Math.Atan((ArrowLine.Y2 - ArrowLine.Y1) / (ArrowLine.X2 - ArrowLine.X1)) + (x1 < x2 ? (Math.PI * 3 / 4) : (Math.PI * 1 / 4))) * 15;
+            
+            ArrowHead2.X1 = endPoint.X;
+            ArrowHead2.Y1 = endPoint.Y;
+
+            ArrowHead2.X2 = ArrowHead2.X1 + Math.Cos(Math.Atan((ArrowLine.Y2 - ArrowLine.Y1) / (ArrowLine.X2 - ArrowLine.X1)) - (x1 < x2 ? (Math.PI * 3 / 4) : (Math.PI * 1 / 4))) * 15;
+            ArrowHead2.Y2 = ArrowHead2.Y1 + Math.Sin(Math.Atan((ArrowLine.Y2 - ArrowLine.Y1) / (ArrowLine.X2 - ArrowLine.X1)) - (x1 < x2 ? (Math.PI * 3 / 4) : (Math.PI * 1 / 4))) * 15;
+        }
+
+        private Point GetLocalPosFromMainCanvas(Point point)
+        {
+            return new Point(point.X - Canvas.GetLeft(this), point.Y - Canvas.GetTop(this));
+        }
+    }
+}
---
+++ wpf/MattNode/MattNode/MattNode/Main/NodeArrow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MattNode
+{
+    /// <summary>
+    /// NodeArrow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class NodeArrow : Arrow
+    {
+        private Node StartNode;
+        private Node EndNode;
+        public NodeArrow(Node startNode, Node endNode)
+        {
+            StartNode = startNode;
+            EndNode = endNode;
+            InitializeComponent();
+            HorizontalAlignment = HorizontalAlignment.Left;
+            VerticalAlignment = VerticalAlignment.Top;
+
+            ArrowLine = arrowLine;
+            ArrowHead1 = arrowHead1;
+            ArrowHead2 = arrowHead2;
+
+            SetArrow();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            StartNode.ArrowsFromMe.Remove(this);
+            EndNode.ArrowsFromOther.Remove(this);
+        }
+
+        public void SetArrow()
+        {
+            double x1 = StartNode.Margin.Left + StartNode.Width / 2;
+            double y1 = StartNode.Margin.Top + StartNode.Height / 2;
+
+            double x2 = EndNode.Margin.Left + EndNode.Width / 2;
+            double y2 = EndNode.Margin.Top + EndNode.Height / 2;
+
+            double margin = 10;
+
+            double left = EndNode.Margin.Left - margin;
+            double right = EndNode.Margin.Left + EndNode.Width + margin;
+            double top = EndNode.Margin.Top - margin;
+            double bottom = EndNode.Margin.Top + EndNode.Height + margin;
+
+            double goalX = x2;
+            double goalY = y2;
+
+            if (x1 < x2)
+            {
+                double yAtLeft = GetYInLine(left, x1, y1, x2, y2);
+                if (top <= yAtLeft && yAtLeft <= bottom)
+                {
+                    goalX = left;
+                    goalY = yAtLeft;
+                }
+                else
+                {
+                    if(y1 < y2)
+                    {
+                        goalX = GetXInLine(top, x1, y1, x2, y2);
+                        goalY = top;
+                    }
+                    else
+                    {
+                        goalX = GetXInLine(bottom, x1, y1, x2, y2);
+                        goalY = bottom;
+                    }
+                }
+            }
+            else
+            {
+                double yAtRight = GetYInLine(right, x1, y1, x2, y2);
+                if (top <= yAtRight && yAtRight <= bottom)
+                {
+                    goalX = right;
+                    goalY = yAtRight;
+                }
+                else
+                {
+                    if (y1 < y2)
+                    {
+                        goalX = GetXInLine(top, x1, y1, x2, y2);
+                        goalY = top;
+                    }
+                    else
+                    {
+                        goalX = GetXInLine(bottom, x1, y1, x2, y2);
+                        goalY = bottom;
+                    }
+                }
+            }
+
+            SetArrow(x1, y1, goalX, goalY);
+        }
+
+        private Point GetLocalPosFromMainCanvas(Point point)
+        {
+            return new Point(point.X - Canvas.GetLeft(this), point.Y - Canvas.GetTop(this));
+        }
+
+        private double GetYInLine(double x, double x1, double y1, double x2, double y2)
+        {
+            return (x-x1)*(y2-y1)/(x2-x1) + y1;
+        }
+        private double GetXInLine(double y, double x1, double y1, double x2, double y2)
+        {
+            return (y-y1)*(x2-x1)/(y2-y1) + x1;
+        }
+    }
+}
