b99edcc37eab34c5951daa63f45824a369e66bd1
matt <matt@physia.kr>
2024-08-09 23:25:00 +0900

    노드 생성 버튼 및 퍼포먼스 테스트 버튼 제거

--- wpf/MattNode/MattNode/MattNode/obj/Debug/net8.0-windows/MattNode.AssemblyInfo.cs
+++ wpf/MattNode/MattNode/MattNode/obj/Debug/net8.0-windows/MattNode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MattNode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1826e5723b958cef1701c9f964bcbc1167197705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cfb5811c79a2db2d833ef70ee3cfe590f9eda5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MattNode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MattNode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
